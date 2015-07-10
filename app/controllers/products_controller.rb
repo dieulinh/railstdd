7160694842f0852cf3b6219b05714ef78a435f40
dieulinh <linhvn09@gmail.com>
2015-07-10 14:36:40 +0700

    test product controller with rspec

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,47 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all
 	end
+
+	def new
+		@product = Product.new
+	end
+	def destroy
+		@product = Product.find(params[:id])
+		@product.destroy
+		redirect_to products_url
+	end
+	def edit
+		@product = Product.find(params[:id])
+
+		render :new
+	end
+
+	def create
+		@product = Product.new(product_params)
+		if @product.save
+			redirect_to products_url
+		else
+
+			render :new
+		end
+	end
+
+	def update
+		@product = Product.find(params[:id])
+		if @product.update(product_params)
+			redirect_to products_url
+		else
+
+		end
+	end
+
+	private
+
+		def product_id
+			params[:product][:id]
+		end
+
+		def product_params
+			params.require(:product).permit(:title, :description, :price)
+		end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,14 +1,69 @@
 require 'rails_helper'
 RSpec.describe ProductsController, type: :controller do
-	context "#index" do
-		let!(:new_created_products) { FactoryGirl.create_list(:product, 3) }
-	  describe "get" do
-      describe "Check if it contains products" do
-        it "contains 2 products" do
-        	get :index
-          expect(assigns(:products).count).to be 3
-        end
-      end
-   end
+	context "User can CRUD Products" do
+		describe "#index" do
+			let!(:new_created_products) { FactoryGirl.create_list(:product, 3) }
+	      
+	    it " it must contains products" do
+	    	get :index
+	      expect(assigns(:products).count).to be 3
+	    end
+
+		end
+
+	describe "#new" do
+		it "should be a product" do
+			get :new
+			expect(assigns(:product)).to be_a_new Product
+		end
+	end
+
+	describe "#create" do
+		let!(:product_params) { FactoryGirl.attributes_for(:product) }
+		it "should create a product" do
+			puts product_params
+			expect{
+				post :create, product: product_params
+				}.to change(Product, :count).by(1)
+		end
+	end
+
+	describe "#edit" do
+		let!(:product) { FactoryGirl.create(:product) }
+	  it "should render a new view ready for update" do
+	  	expect(get :edit, id: product.id ).to render_template(:new)
+		end
+		it "should have product available for render" do
+			get :edit, id: product.id
+	  	expect(assigns(:product)).to eq (product)
+		end
+	end
+
+	describe "#update" do
+		let!(:product) { FactoryGirl.create(:product) }
+		
+		describe "new value should be updated" do
+			let!(:new_product_params) { {title: "Python book for you", 
+																	description: "For you learning Python in 10 years",
+																	price: 134 } }
+			before{
+  			put :update, id: product.id, product: new_product_params
+  			product.reload
+		  }																	
+		  it "update new title should work" do
+		  	expect(product.description).to include("For you learning Python in 10 years")
+		  	expect(product.price).to eq 134
+		  end
+		end
+	end
+
+	describe "#destroy" do
+		let!(:product) { FactoryGirl.create(:product) }
+		it "Table product should descrease by 1" do
+			expect{
+				delete :destroy, id: product.id
+				}.to change(Product, :count).by(-1)
+		end
+	end
 	end
 end
