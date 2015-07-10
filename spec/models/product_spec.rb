7db3c6b7310395f4977fd4ab08e37549e1ddaf37
dieulinh <linhvn09@gmail.com>
2015-07-10 10:55:00 +0700

    refactor product model with rspec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,15 +2,16 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
 
 	context 'Common Validation' do
-		let!(:product) { FactoryGirl.create(:product) }
-		
 		it { should validate_presence_of :title }
 		it { should validate_presence_of :description }
 		it { should validate_numericality_of(:price).is_greater_than(0) }
 		
-		it "Title should be all lowercase" do 
-	  	expect(product.title).to eq product.title.downcase
-	  end
+		context "should be all lowercase for title"
+			let!(:product) { FactoryGirl.create(:product) }
+			it "Title should be all lowercase" do 
+		  	expect(product.title).to eq product.title.downcase
+		  end
+		end
 	end
 
 	context 'Custom Validation' do
